c736b9f674d4110aec6225dc7882081ed1014a97
nicoWeb31 <nico.riot@free.fr>
2021-03-06 20:24:58 +0100

    add map

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,16 @@
-import { User } from './maps/User';
-console.log('testxw')
+import { User } from "./maps/User";
+import { Company } from "./maps/Company";
+console.log("testxw");
 
+// const user = new User();
+// console.log(user)
 
+// const company = new Company();
+// console.log(company);
 
-const user = new User();
-console.log(user)
+const divMap = document.getElementById("map");
+
+const maps = new google.maps.Map(divMap, {
+    zoom: 1,
+    center: { lat: 0, lng: 0 },
+});
