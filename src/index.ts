a3a4eed2da1ce972321f4c99f9e5be20fffa3ff8
nicoWeb31 <nico.riot@free.fr>
2021-03-07 19:22:39 +0100

    add marker

--- src/index.ts
+++ src/index.ts
@@ -1,16 +1,25 @@
 import { User } from "./maps/User";
 import { Company } from "./maps/Company";
+import { CustomMap } from "./maps/CustomMap";
+
 console.log("testxw");
 
-// const user = new User();
-// console.log(user)
+const user = new User();
+console.log(user)
 
-// const company = new Company();
-// console.log(company);
+const company = new Company();
+console.log(company);
+
+// const divMap = document.getElementById("map");
+
+// const map = new google.maps.Map(divMap, {
+//     zoom: 1,
+//     center: { lat: 0, lng: 0 },
+// });
+const map = new CustomMap('map');
+
+map.addMarker(user);
+map.addMarker(company);
 
 
 
-const map = new google.maps.Map(divMap, {
-    zoom: 1,
-    center: { lat: 0, lng: 0 },
-});
--- src/maps/CustomMap.ts
+++ src/maps/CustomMap.ts
@@ -1,13 +1,43 @@
+import { User } from './User';
+import { Company } from './Company';
+
 export class CustomMap {
     private googleMap: google.maps.Map;
     divMap: HTMLElement;
 
-    constructor() {
-        this.divMap = document.getElementById("map"); 
+    constructor(divId: string) {
+        this.divMap = document.getElementById(divId); 
         this.googleMap = new google.maps.Map(this.divMap, {
             zoom: 1,
             center: { lat: 0, lng: 0 },
         });
         ;
     }
+
+
+    addUserMarker(user: User): void {
+        new google.maps.Marker({
+            map: this.googleMap,
+            position: { lat: user.location.lat, lng: user.location.long },
+        })
+    }
+
+
+    addMarkerCompagny(company: Company): void {
+        new google.maps.Marker({
+            map: this.googleMap,
+            position: { lat: company.location.lat, lng: company.location.long },
+        })
+    }
+
+
+    addMarker(marker : User | Company): void {
+        new google.maps.Marker({
+            map: this.googleMap,
+            position: { lat: marker.location.lat, lng: marker.location.long },
+        })
+    }
+
+
+
 }
